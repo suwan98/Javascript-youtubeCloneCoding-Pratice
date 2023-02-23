6d5e55330b5b0a515f46a2011409fabeac368ff3
suwan98 <imissyou5466@gmail.com>
2023-02-23 23:07:45 +0900

    2023-02-23

---
+++ practice/script.js
@@ -0,0 +1,32 @@
+;(function () {
+  ;('use strict')
+
+  const get = (target) => document.querySelector(target)
+  const getAll = (target) => document.querySelectorAll(target)
+
+
+  const $search = get('#search')
+  const $list = getAll('.contents.list figure')
+  const $searchButton = get('.btn-search')
+
+  const init = () => {
+    $search.addEventListener('keyup', search)
+    $search.addEventListener('click', search)
+  }
+
+  const search = () => {
+    let searchText = $search.value.toLowerCase()
+    for (let index =0; index < $list.length; index++){
+      const $target = $list[index].querySelector('strong')
+      const text = $target.textContent.toLowerCase()
+      if(-1 < text.indexOf(searchText)){
+        $list[index].style.display = 'flex'
+      }else{
+        $list[index].style.display = 'none'
+      }
+    }
+
+  }
+
+  init()
+})()
